fd6bf0f3e7dc3957f39fcd424245a6177901ad79
Dev <Dev@MacBook-Pro-de-AGOGUE-2.local>
2021-09-26 11:10:12 +0000

    setup button to display hello screen

--- firstapp/controller/ViewController.swift
+++ firstapp/controller/ViewController.swift
@@ -13,12 +13,23 @@ class ViewController: UIViewController {
     // MARK: - Properties
 
     @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var button: UIButton!
+    
 
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "iOS Essentials"
         label.text = "Hello World from the next iOS rock star"
+        button.addTarget(self, action: #selector(launchHelloVC), for: .touchUpInside)
+    }
+    
+    @objc
+    func launchHelloVC(){
+        let storyBoard = UIStoryboard(name: "HelloViewController", bundle: nil)
+        let helloViewController = storyBoard.instantiateViewController(withIdentifier: "HelloViewController")
+         self.navigationController?.pushViewController(helloViewController, animated: true)
     }
 
 
